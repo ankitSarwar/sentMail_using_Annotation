6e78cd3da00819cfd913ecd3310b3b92ce8ae7bb
Ankit Sarwar <ankitsarwar039@gmail.com>
2023-04-17 09:26:17 +0530

    weekly test

---
+++ src/main/java/MyPackage/man.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class man {
+    public void eating(){
+        System.out.println("I am eating apple ..");
+    }
+}
